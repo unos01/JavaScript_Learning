640cc6b5616cfb46e0b71df337d69f01a255ee29
Sonu Sharma <92368824+unos01@users.noreply.github.com>
2024-03-24 15:29:20 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,22 @@
+const accountId = 1234; //once we declare and assign some value to variable, so we can't  change it
+let accountEmail = "stark@example.com";
+var accountPassword = "123456";
+accountCity = "Mumbai"; //yesa bhi possible hai but don't use every time for your good practice
+let accountState
+
+
+{
+    // this is block that is curly braces
+}
+
+// accountId = 2345; //not allowed
+// console.log(accountId); //output is error if i update the value of the constant variable
+
+accountEmail="abc@gmail.com"
+accountPassword="12345678"
+accountCity="New Mumbai"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// prefer not to use var 
+// because of issue in block scope and functon scope
