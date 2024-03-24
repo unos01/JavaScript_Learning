f7cf1d74406013e22f1a16fa2ced8f241abbe32d
Sonu Sharma <92368824+unos01@users.noreply.github.com>
2024-03-24 16:00:06 +0000

    dataTypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,24 @@
+"use strict" //treat all js code as newer version or treat as strict mode to reactify the mistecs 
+
+// alert("hello"); // this is pop up this can be use for window not for  nodejs
+
+let name="sonu"
+let age =23;
+let isLoggedIn=true
+
+
+//data type
+//1 number => 2 to power 53
+//2 bigint
+//3 string=> ""
+//4 boolean => true/false
+//5 null => standalone value
+//6 undefined => 
+//7 symbol => use for unique
+//8 object
+
+console.log(name);
+console.log(typeof name);
+console.log(typeof "Sonu");
+console.log(typeof null);
+console.log(typeof undefined);
